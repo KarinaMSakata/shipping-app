6da7c189539b7d7e998474931c2b542478c69774
Karina Sakata <karina.msakata@hotmail.com>
2022-10-04 19:30:23 -0300

    Autenticação Criada

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_02_143307) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_04_163238) do
   create_table "mode_of_transports", force: :cascade do |t|
     t.string "name"
     t.integer "max_distance"
@@ -20,6 +20,20 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_02_143307) do
     t.integer "fixed_rate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "role", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
